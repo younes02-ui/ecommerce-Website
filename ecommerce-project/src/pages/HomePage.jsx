af8a38099cc4c686026c7349b61f7e634228e90f
younes02-ui <ky_boudjella@esi.dz>
2026-02-19 13:32:02 -0500

    use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import './HomePage.css'
 import Header from '../components/Header'
 import { products } from '../../data/products';
@@ -6,13 +7,11 @@ import { products } from '../../data/products';
 
 function HomePage() {
 
-    fetch('http://localhost:3000/api/products')
+    axios.get('http://localhost:3000/api/products')
         .then((response) => {
-            return response.json()
-                .then((data) => {
-                    console.log(data)
-                })
-        })
+            console.log(response.data);
+        });
+
 
     return (
         <>

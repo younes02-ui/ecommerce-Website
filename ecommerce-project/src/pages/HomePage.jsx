8009ff940eca0b5aab70df89a3840bfcc6e9492e
younes02-ui <ky_boudjella@esi.dz>
2026-02-19 11:58:27 -0500

    Move checkout page into react

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,10 @@ import './header.css'
 function HomePage() {
   return (
     <>
+
+        <title>Ecommerce Project</title>
+
+
         <div className="header">
         <div className="left-section">
             <a href="index.html" className="header-link">

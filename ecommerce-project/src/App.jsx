0adc37bf595973ea224b71316cb3a127dc56d4ef
younes02-ui <ky_boudjella@esi.dz>
2026-02-19 12:17:49 -0500

    add Tracking and Orders Route

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage'
 import CheckoutPage from './pages/CheckoutPage'
+import Orders from './pages/Orders'
+import Tracking from './pages/Tracking'
 import './App.css'
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<Orders />} />
+      <Route path="tracking" element={<Tracking />} />
     </Routes>
   )
 }

7942063c4ef5548aa685c17ac92911aa751fbc81
younes02-ui <ky_boudjella@esi.dz>
2026-02-20 10:37:30 -0500

    add server proxy confug for /api and /images

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -10,13 +10,13 @@ function HomePage() {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
-         axios.get('http://localhost:3000/api/products')
+         axios.get('/api/products')
         .then((response) => {
             setProducts(response.data);
         });
 
 
-        axios.get('http://localhost:3000/api/cart-items')
+        axios.get('/api/cart-items')
         .then((response)=> {
               setCart(response.data);
         })
